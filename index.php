eeb3be11c64db4c0c2ab2248b3f2a2764e1eece7
leoncci <leon.morival@gmail.com>
2024-11-07 13:45:20 +0100

    tailwind added

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <link href="./css/output.css" rel="stylesheet">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tp php</title>
 </head>
 <?php require_once('./header.php') ?>
 <body>
-   
+   <h1 class="bg-black text-4xl">sdqf</h1>
 </body>
-<?php require_once('./footer.php') ?>
+
 </html>
