25f55e9db5014fec9a833f4e45a950e966da23b1
leoncci <leon.morival@gmail.com>
2024-11-07 16:12:02 +0100

    insert into db

--- data.php
+++ data.php
@@ -1,25 +1,22 @@
 <?php
 require_once("./connect.php");
-session_start();
 
 
 
-if (!isset($_SESSION['contacts'])) {
-    $_SESSION['contacts'] = [];
-}
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = isset($_POST['name']) ? $_POST['name'] : '';
     $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : '';
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     if (!empty($name) && !empty($telephone) && !empty($email)) {
-        $_SESSION['contacts'][] = ["name" => $name, "telephone" => $telephone, "email" => $email];
+        $insert = $conn->query("INSERT INTO `user` (`name`, `email`, `telephone`) VALUES ('$name', '$email', '$telephone');");
     }
 }
 
+
 function afficherContact($conn) {
     $data = $conn->query("SELECT * FROM user");
-    if (isset($_SESSION['contacts']) && !empty($_SESSION['contacts'])) {
+    if ( !empty($data)) {
         foreach ($data as $row) {
             echo "<tr>";
             echo "<td class='px-4 py-2 border'>" . $row['name']. "</td>";
